dabfc823c5375718c1302d6037f4a75134b49ba5
Joe Landers <jcl696@stern.nyu.edu>
2019-06-11 18:47:26 -0400

    Basic Requirements
    Documenting the basic requirements needed in the receipt / assignment

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,16 @@
 
 
+# Basic Requirements 
+
+#A grocery store name of your choice
+#A grocery store phone number and/or website URL and/or address of choice
+#The date and time of the beginning of the checkout process, formatted in a human-friendly way(e.g. 2019-06-06 11: 31 AM)
+#The name and price of each shopping cart item, price being formatted as US dollars and cents(e.g. $1.50)
+#The total cost of all shopping cart items, formatted as US dollars and cents(e.g. $4.50), calculated as the sum of their prices
+#The amount of tax owed(e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75 % (for the purposes of this project, groceries are not exempt from sales tax)
+#The total amount owed, formatted as US dollars and cents(e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+#A friendly message thanking the customer and/or encouraging the customer to shop again
+#
 
 
 products = [
