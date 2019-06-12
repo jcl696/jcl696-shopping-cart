15d1962f100eff43f66c8f1a91aa89283bec665e
Joe Landers <jcl696@stern.nyu.edu>
2019-06-11 22:37:18 -0400

    for loop on Selected_ids
    running for loop on newly created list, making it into a dictionary, then running the total price (adding it up) and printing each choice we make

--- shopping_cart.py
+++ shopping_cart.py
@@ -66,6 +66,7 @@ products = [
 #Info Capture / Inputs
 
 total_price = 0
+selected_ids = []
 
 while True:
      selected_id=input("Please input a product identifier: ")  # (string)
@@ -73,13 +74,18 @@ while True:
      if selected_id == "DONE":
          break
      else:
-         matching_products=[p for p in products if str(p["id"]) == str(selected_id)]  # matching_products = list
-         matching_product=matching_products[0]
-         total_price = total_price + matching_product["price"]
-         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
+         
+         selected_ids.append(selected_id)
 
 ## Info Display /  Outputs
+
+#print(selected_ids)
+
+for selected_id in selected_ids:
+     matching_products=[p for p in products if str(p["id"]) == str(selected_id)]  # matching_products = list
+     matching_product = matching_products[0]
+     total_price = total_price + matching_product["price"]
+     print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
 print("TOTAL PRICE:" + " " + str(total_price)) #TODO --> Format as USD 
