4744a0e5dd7e21d53436dd10370dc4ee9e5d331d
Joe Landers <jcl696@stern.nyu.edu>
2019-06-11 22:24:46 -0400

    Adding up price in while Loop

--- shopping_cart.py
+++ shopping_cart.py
@@ -12,7 +12,6 @@
 #A friendly message thanking the customer and/or encouraging the customer to shop again
 #
 
-## Info Display /  Outputs
 
 
 products = [
@@ -66,6 +65,7 @@ products = [
 
 #Info Capture / Inputs
 
+total_price = 0
 
 while True:
      selected_id=input("Please input a product identifier: ")  # (string)
@@ -75,7 +75,19 @@ while True:
      else:
          matching_products=[p for p in products if str(p["id"]) == str(selected_id)]  # matching_products = list
          matching_product=matching_products[0]
+         total_price = total_price + matching_product["price"]
          print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+
+## Info Display /  Outputs
+
+
+print("TOTAL PRICE:" + " " + str(total_price)) #TODO --> Format as USD 
+
+
+
+
+
 
 #selected_id = input("Please input a product identifier: ")  # (string)
 #
@@ -87,9 +99,4 @@ while True:
 
 
 
-#look up a product for a given input
-
-
-#print(selected_id)
-
-#print(type(selected_id))
+ 
